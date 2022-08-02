e00f27a1a9843d7fd008fbb5de350fa1b1864a9c
Lucas Yule <lyrma@cin.ufpe.br>
2022-08-02 14:27:50 -0300

    final commit

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,23 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalBody
+          mx="auto"
+          w="auto"
+          h="auto"
+          maxW={['300px', '500px', '900px']}
+          maxH={['350px', '450px', '600px']}
+        >
+          <Image src={imgUrl} />
+        </ModalBody>
+        <ModalFooter bgColor="gray.600">
+          <Link href={imgUrl}>Abrir original</Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
@@ -7,6 +8,19 @@ import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+
+type Image = {
+  title: string;
+  url: string;
+  description: string;
+  ts: number;
+  id: string;
+};
+
+type GetImagesResponse = {
+  after: string;
+  data: Image[];
+};
 
 export default function Home(): JSX.Element {
   const {
@@ -19,17 +33,40 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    async ({ pageParam = null }): Promise<GetImagesResponse> => {
+      const { data } = await api.get('/api/images', {
+        params: {
+          after: pageParam,
+        },
+      });
+
+      return data;
+    },
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: lastPage => {
+        return lastPage?.after || null;
+      },
+    }
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    const flattenDataArray = data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    });
+    return flattenDataArray;
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
 
   // TODO RENDER ERROR SCREEN
+  if (isError && !isLoading) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -38,6 +75,21 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            onClick={() => {
+              fetchNextPage();
+            }}
+            isLoading={isFetchingNextPage}
+            loadingText="Carregando..."
+            variantColor="teal"
+            variant="outline"
+            size="lg"
+            mt={20}
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
